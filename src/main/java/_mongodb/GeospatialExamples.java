56a2a1482a9618e776d9175d634cc09bbb5b41c3
autfish <autfish@163.com>
2016-04-28 21:33:31 +0800

    mongodb geo

--- src/main/java/_mongodb/GeospatialExamples.java
+++ src/main/java/_mongodb/GeospatialExamples.java
@@ -5,9 +5,16 @@ import java.util.Arrays;
 
 import org.bson.Document;
 
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.geojson.Point;
+import com.mongodb.client.model.geojson.Polygon;
+import com.mongodb.client.model.geojson.Position;
+
 import static com.mongodb.client.model.Indexes.*;
 
 public class GeospatialExamples {
@@ -30,13 +37,36 @@ public class GeospatialExamples {
 		MongoCollection<Document> mc = database.getCollection("people");
 		mc.drop();
 		
-		Document doc1 = new Document("name", "xiao ming").append("location", Arrays.asList(1, 100)).append("loc", new Document("type", "Point").append("coordinates", Arrays.asList(10, 10)));
-		Document doc2 = new Document("name", "xiao hong").append("location", Arrays.asList(10, 30)).append("loc", new Document("type", "Point").append("coordinates", Arrays.asList(10, 10)));
-		Document doc3 = new Document("name", "da pang").append("location", Arrays.asList(180, 5)).append("loc", new Document("type", "Point").append("coordinates", Arrays.asList(10, 10)));
-		Document doc4 = new Document("name", "xiao hu").append("location", Arrays.asList(50, 50)).append("loc", new Document("type", "Point").append("coordinates", Arrays.asList(10, 10)));
+		Document doc1 = new Document("name", "xiao ming").append("loc1", Arrays.asList(1.1, 1.1)).append("loc2", new Point(new Position(10, 10)));
+		Document doc2 = new Document("name", "xiao hong").append("loc1", Arrays.asList(2.1, 2.1)).append("loc2", new Point(new Position(10.1, 10.1)));
+		Document doc3 = new Document("name", "da pang").append("loc1", Arrays.asList(3.1, 3.1)).append("loc2", new Point(new Position(10.2, 10.2)));
+		Document doc4 = new Document("name", "xiao hu").append("loc1", Arrays.asList(4.1, 4.1)).append("loc2", new Point(new Position(10.3, 10.3)));
 		mc.insertMany(Arrays.asList(doc1, doc2, doc3, doc4));
 		
-		mc.createIndex(geo2d("location"));
-		mc.createIndex(geo2dsphere("loc"));
+		mc.createIndex(geo2d("loc1"));
+		mc.createIndex(geo2dsphere("loc2"));
+		
+		//geoWithin 正方形
+		FindIterable<Document> iterable = mc.find(Filters.geoWithin("loc1", new Polygon(Arrays.asList(new Position(0, 0), new Position(4, 0), new Position(4, 4), new Position(0, 4), new Position(0, 0)))));
+		printResult("Filters.geoWithin", iterable);
+		
+		//near
+		iterable = mc.find(Filters.near("loc2", new Point(new Position(10, 10)), 16000d, 0d));
+		printResult("Filters.near", iterable);
+		
+		//geoWithinCenter
+		iterable = mc.find(Filters.geoWithinCenter("loc1", 0d, 0d, 3.5));
+		printResult("Filters.geoWithinCenter", iterable);
+	}
+	
+	public void printResult(String doing, FindIterable<Document> iterable) {
+		System.out.println(doing);
+        iterable.forEach(new Block<Document>() {
+            public void apply(final Document document) {
+                System.out.println(document);
+            }
+        });
+        System.out.println("------------------------------------------------------");
+        System.out.println();
 	}
 }
