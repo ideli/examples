a9a2da6494e3f14c1e4851e0fff3c34e435e20a6
autfish <autfish@163.com>
2016-05-06 23:49:15 +0800

    mongodb

--- src/main/java/_mongodb/FindExamples.java
+++ src/main/java/_mongodb/FindExamples.java
@@ -10,6 +10,8 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+
 import static com.mongodb.client.model.Projections.*;
 import static com.mongodb.client.model.Sorts.*;
 
@@ -30,32 +32,80 @@ public class FindExamples {
 	}
 	
 	public void beginShow() {
-		MongoCollection<Document> mc = database.getCollection("fruits");
+		MongoCollection<Document> mc = database.getCollection("blog");
 		mc.drop();
 		
-		Document doc1 = new Document("name", "apple").append("color", "red").append("price", 10.0);
-		Document doc2 = new Document("name", "orange").append("color", "orange").append("price", 7.99);
-		Document doc3 = new Document("name", "banana").append("color", "yellow").append("price", 2.99);
-		Document doc4 = new Document("name", "strawberry").append("color", "red").append("price", 14.99);
-		mc.insertMany(Arrays.asList(doc1, doc2, doc3, doc4));
+		Document doc1 = new Document("title", "good day").append("author", "tom").append("words", 300)
+				.append("comments", Arrays.asList(new Document("author", "joe").append("score", 3).append("comment", "good"), new Document("author", "white").append("score", 1).append("comment", "oh no")));
+		Document doc2 = new Document("title", "good").append("author", "john").append("words", 400)
+				.append("comments", Arrays.asList(new Document("author", "william").append("score", 4).append("comment", "good"), new Document("author", "white").append("score", 6).append("comment", "very good")));
+		Document doc3 = new Document("title", "good night").append("author", "mike").append("words", 200);
+		Document doc4 = new Document("title", "happiness").append("author", "tom").append("words", 1480);
+		Document doc5 = new Document("title", "a good thing").append("author", "tom").append("words", 180);
+		mc.insertMany(Arrays.asList(doc1, doc2, doc3, doc4, doc5));
+		//单字段索引
+		mc.createIndex(new Document("words", 1));
+		//组合索引
+		mc.createIndex(new Document("title", 1).append("author", -1));
 		
-		mc.createIndex(new Document("price", 1));
-		mc.createIndex(new Document("color", 1).append("name", -1));
-		
-		FindIterable<Document> iterable = mc.find().sort(ascending("name"));
+		//查询全部
+		FindIterable<Document> iterable = mc.find();
 		printResult("find all", iterable);
+		
+		//查询title=good
+        iterable = mc.find(new Document("title", "good"));
+        printResult("find title=good", iterable);
         
-        iterable = mc.find(new Document("color", "red"));
-        printResult("find color:red", iterable);
+      //查询title=good and author=tom
+        iterable = mc.find(new Document("title", "good").append("author", "tom"));
+        printResult("find title=good and author=tom", iterable);
+		
+		//查询全部按title排序
+		iterable = mc.find().sort(ascending("title"));
+		printResult("find all ascending title", iterable);
+		
+		//查询全部按author,title排序
+		iterable = mc.find().sort(ascending("author", "title"));
+		printResult("find all ascending author,title", iterable);
+		
+		//查询全部按words倒序排序
+		iterable = mc.find().sort(descending("words"));
+		printResult("find all descending words", iterable);
+
+        //查询author=tom or words>350
+        iterable = mc.find(new Document("$or", Arrays.asList(new Document("author", "tom"), new Document("words", new Document("$gt", 350)))));
+        printResult("find author=tom or words>350", iterable);
         
-        iterable = mc.find(new Document("color", "red").append("name", "strawberry"));
-        printResult("find color:red and name:strawberry", iterable);
+        //返回title和author字段
+        iterable = mc.find().projection(include("title", "author"));
+        printResult("find all include (title,author)", iterable);
         
-        iterable = mc.find(new Document("$or", Arrays.asList(new Document("color", "red"), new Document("price", new Document("$gt", 8)))));
-        printResult("find color:red or price>8", iterable);
+        //返回除title外的其他字段
+        iterable = mc.find().projection(exclude("title"));
+        printResult("find all exclude title", iterable);
         
-        iterable = mc.find(include("apple"));
-        printResult("find include(\"color\")", iterable);
+        //不返回_id字段
+        iterable = mc.find().projection(excludeId());
+        printResult("find all excludeId", iterable);
+        
+      //返回title和author字段
+        iterable = mc.find().projection(fields(include("title", "author"), excludeId()));
+        printResult("find all include (title,author) and excludeId", iterable);
+        
+        //内嵌文档匹配
+        iterable = mc.find(new Document("comments.author", "joe"));
+        printResult("find comments.author=joe", iterable);
+        
+        //一个错误的示例, 想查询评论中包含作者是white且分值>2的, 返回结果不符合预期
+        iterable = mc.find(new Document("comments.author", "white").append("comments.score", new Document("$gt", 2)));
+        printResult("find comments.author=white and comments.score>2 (wrong)", iterable);
+        
+        //正确的写法
+        iterable = mc.find(elemMatch("comments", Filters.and(Filters.eq("author", "white"), Filters.gt("score", 2))));
+        printResult("find comments.author=white and comments.score>2 using elemMatch", iterable);
+        
+        iterable = mc.find(Filters.regex("title", "^good")).projection(slice("comments", 1));
+        printResult("find all excludeId", iterable);
 	}
 	
 	public void printResult(String doing, FindIterable<Document> iterable) {
