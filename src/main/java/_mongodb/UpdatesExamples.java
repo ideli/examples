0d1403a4ad769f8cb59a3e311a1c9e5f491fbe66
autfish <autfish@163.com>
2016-05-09 23:09:38 +0800

    mongodb

--- src/main/java/_mongodb/UpdatesExamples.java
+++ src/main/java/_mongodb/UpdatesExamples.java
@@ -13,6 +13,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.PushOptions;
+import com.mongodb.client.model.UpdateOptions;
 
 import static com.mongodb.client.model.Updates.*;
 
@@ -38,12 +39,11 @@ public class UpdatesExamples {
 		mc.drop();
 		printCollection("drop", mc);
 		
-		//插入单个文档
-		Document doc = new Document("oop", "java");
-		mc.insertOne(doc);
+		mc.insertOne(new Document("oop", "java"));
 		printCollection("insert java", mc);
 		
-		doc = new Document("oop", "csharp").append("copyright", "microsoft");
+		//插入一个两个字段的文档
+		Document doc = new Document("oop", "csharp").append("copyright", "microsoft");
 		mc.insertOne(doc);
 		printCollection("insert csharp", mc);
 		
@@ -59,40 +59,36 @@ public class UpdatesExamples {
 		mc.deleteMany(new Document());
 		printCollection("delete all", mc);
 		
-		doc = new Document("oop", "java").append("copyright", "oracle");
-		mc.insertOne(doc);
-		doc = new Document("oop", "csharp").append("copyright", "microsoft");
-		mc.insertOne(doc);
-		printCollection("insert java and csharp", mc);
+		//重新插入测试文档
+		mc.insertOne(new Document("oop", "java").append("copyright", "oracle"));
+		mc.insertOne(new Document("oop", "csharp").append("copyright", "microsoft"));
+		printCollection("insert java,csharp and swift", mc);
 		
-		//$set 有则修改,没有则添加
-		mc.updateOne(new Document("oop", "java"), set("rank", 100));
-		printCollection("$set java rank 100", mc);
-		//$inc 有则增加,没有则添加
+		//$set 文档中存在指定字段则修改,没有则添加
+		mc.updateMany(new Document(), set("rank", 100));
+		printCollection("$set all rank 100", mc);
+		
+		//$unset 文档中存在指定字段则删除该字段
+		mc.updateOne(new Document("oop", "csharp"), unset("rank"));
+		printCollection("unset csharp rank", mc);
+		
+		//$inc 文档中存在指定字段则增加,没有则添加
 		mc.updateOne(new Document("oop", "csharp"), inc("rank", 30));
 		printCollection("$inc csharp rank 30", mc);
 		mc.updateOne(new Document("oop", "csharp"), inc("rank", 31));
 		printCollection("$inc csharp rank 31", mc);
 		
-		//$unset 有则删除
-		doc = new Document("oop", "swift").append("copyright", "apple");
-		mc.insertOne(doc);
-		printCollection("insert swift", mc);
-		mc.updateMany(new Document(), unset("copyright"));
-		printCollection("unset all copyright", mc);
+		//$setOnInsert 在更新时指定upsert=true并实际触发了插入操作时生效
+		mc.updateOne(new Document("oop", "swift").append("copyright", "apple"), setOnInsert("rank", 100), new UpdateOptions().upsert(true));
+		printCollection("$setOnInsert rank 100 for swift", mc);
 		
 		//$mul 相乘
-		mc.updateOne(new Document("oop", "java"), set("rank", 100));
-		printCollection("$set java rank: 100", mc);
-		
 		mc.updateOne(new Document("oop", "java"), mul("rank", 0.2));
 		printCollection("$mul java rank: 0.2", mc);
 		
-		mc.updateOne(new Document("oop", "swift"), set("rank", 100));
-		
-		//$rename
+		//$rename 重命名
 		mc.updateMany(new Document(), rename("rank", "ranks"));
-		printCollection("$rename java rank to ranks", mc);
+		printCollection("$rename all rank to ranks", mc);
 		
 		//$min 取当前值和指定值之间比较小的
 		mc.updateMany(new Document(), min("ranks", 50));
@@ -110,11 +106,13 @@ public class UpdatesExamples {
 		mc.updateMany(new Document("oop", "java"), currentTimestamp("lastModified"));
 		printCollection("$currentTimestamp java", mc);
 		
-		//$addToSet
+		//$addToSet 添加一个元素到不重复集合
+		mc.updateMany(new Document("oop", "java"), addToSet("keywords", "for"));
 		mc.updateMany(new Document("oop", "java"), addToSet("keywords", "for"));
 		printCollection("$addToSet java keywords: for", mc);
 		
-		//$addEachToSet
+		//$addEachToSet 添加一组元素到不重复集合
+		mc.updateMany(new Document("oop", "java"), addEachToSet("keywords", Arrays.asList("while", "true", "do", "new", "override")));
 		mc.updateMany(new Document("oop", "java"), addEachToSet("keywords", Arrays.asList("while", "true", "do", "new", "override")));
 		printCollection("$addEachToSet java keywords: while,true,do,new,override", mc);
 		
@@ -130,45 +128,42 @@ public class UpdatesExamples {
 		mc.updateMany(new Document("oop", "java"), pull("keywords", "new"));
 		printCollection("$pull java keywords: new", mc);
 		
-		//$pullByFilter
+		//$pullByFilter 根据Filters删除
 		mc.updateMany(new Document("oop", "java"), pullByFilter(Filters.gte("keywords", "true")));
 		printCollection("$pullByFilter java keywords: true", mc);
 		
-		//$pullAll 删除全部
+		//$pullAll 删除一组元素
 		mc.updateMany(new Document("oop", "java"), pullAll("keywords", Arrays.asList("while", "true", "do", "new", "override")));
 		printCollection("$pullAll java keywords", mc);
-		
-		//如果存在则不会重复添加
-		mc.updateMany(new Document("oop", "java"), addEachToSet("keywords", Arrays.asList("while", "true", "do", "new", "override")));
-		mc.updateMany(new Document("oop", "java"), addEachToSet("keywords", Arrays.asList("while", "true", "do", "new", "override")));
-		printCollection("$addEachToSet java keywords: while,true,do,new,override", mc);
-		
-		//$push
+
+		//$push 添加一个元素到可重复集合
 		mc.updateMany(new Document("oop", "java"), push("scores", 89));
 		printCollection("$push java scores: 89", mc);
 		
-		//$pushEach
+		//$pushEach 添加一组元素到可重复集合
 		mc.updateMany(new Document("oop", "java"), pushEach("scores", Arrays.asList(89, 90, 92)));
 		printCollection("$pushEach java scores: 89,90,92", mc);
 		
+		//在集合的指定位置插入一组元素
 		mc.updateMany(new Document("oop", "java"), pushEach("scores", Arrays.asList(11, 12, 13), new PushOptions().position(0)));
 		printCollection("$pushEach java scores: 11,12,13 at position 0", mc);
 		
-		//倒序排列
+		//在集合的指定位置插入一组元素并倒序排列
 		mc.updateMany(new Document("oop", "java"), pushEach("scores", Arrays.asList(40, 41), new PushOptions().sort(-1)));
 		printCollection("$pushEach java scores: 40,41 and sort(-1)", mc);
 		
-		//倒序排列并只保留前3个
+		//在集合的指定位置插入一组元素, 倒序排列后保留前3个
 		mc.updateMany(new Document("oop", "java"), pushEach("scores", Arrays.asList(60, 61), new PushOptions().sort(-1).slice(3)));
 		printCollection("$pushEach java scores: 60,61 and sort(-1) and slice(3)", mc);
-		//
+		
+		//插入一组内嵌文档
 		Bson bson = pushEach("experts",
                 Arrays.asList(new Document("first", "Rod").append("last", "Johnson"),
                               new Document("first", "Doug").append("last", "Cutting")));
 		mc.updateOne(new Document("oop", "java"), bson);
 		printCollection("$pushEach", mc);
 		
-		//combine
+		//combine 组合Bson
 		bson = combine(set("author", "James Gosling"), set("version", "8.0"));
 		mc.updateOne(new Document("oop", "java"), bson);
 		printCollection("$combine", mc);
