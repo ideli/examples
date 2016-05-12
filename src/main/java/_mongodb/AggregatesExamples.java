f53784a7e32d647724220f52a14023dc077f1ec4
autfish <autfish@163.com>
2016-05-12 12:53:46 +0800

    mongodb

--- src/main/java/_mongodb/AggregatesExamples.java
+++ src/main/java/_mongodb/AggregatesExamples.java
@@ -24,93 +24,154 @@ public class AggregatesExamples {
 	public static void main(String[] args) throws ParseException {
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
 		MongoDatabase database = mongoClient.getDatabase("lesson");
-		
+
 		AggregatesExamples client = new AggregatesExamples(database);
 		client.beginShow();
 		mongoClient.close();
 	}
-	
+
 	private MongoDatabase database;
+
 	public AggregatesExamples(MongoDatabase database) {
 		this.database = database;
 	}
-	
+
 	public void beginShow() {
 		MongoCollection<Document> mc = database.getCollection("blog");
 		mc.drop();
-		
-		Document doc1 = new Document("title", "good day").append("author", "tom").append("words", 300)
-				.append("comments", Arrays.asList(new Document("author", "joe").append("score", 3).append("comment", "good"), new Document("author", "white").append("score", 1).append("comment", "oh no")));
-		Document doc2 = new Document("title", "good").append("author", "john").append("words", 400)
-				.append("comments", Arrays.asList(new Document("author", "william").append("score", 4).append("comment", "good"), new Document("author", "white").append("score", 6).append("comment", "very good")));
-		Document doc3 = new Document("title", "good night").append("author", "mike").append("words", 200);
-		Document doc4 = new Document("title", "happiness").append("author", "tom").append("words", 1480);
-		Document doc5 = new Document("title", "a good thing").append("author", "tom").append("words", 180);
+
+		// 插入用于测试的文档
+		Document doc1 = new Document("title", "good day")
+				.append("owner", "tom")
+				.append("words", 300)
+				.append("comments",
+						Arrays.asList(
+								new Document("author", "joe")
+										.append("score", 3).append("comment",
+												"good"),
+								new Document("author", "white").append("score",
+										1).append("comment", "oh no")));
+		Document doc2 = new Document("title", "good")
+				.append("owner", "john")
+				.append("words", 400)
+				.append("comments",
+						Arrays.asList(
+								new Document("author", "william").append(
+										"score", 4).append("comment", "good"),
+								new Document("author", "white").append("score",
+										6).append("comment", "very good")));
+		Document doc3 = new Document("title", "good night")
+				.append("owner", "mike").append("words", 200)
+				.append("tag", Arrays.asList(1, 2, 3, 4));
+		Document doc4 = new Document("title", "happiness")
+				.append("owner", "tom").append("words", 1480)
+				.append("tag", Arrays.asList(2, 3, 4));
+		Document doc5 = new Document("title", "a good thing")
+				.append("owner", "tom").append("words", 180)
+				.append("tag", Arrays.asList(1, 2, 3, 4, 5));
 		mc.insertMany(Arrays.asList(doc1, doc2, doc3, doc4, doc5));
-		
-		//类似于select author,sum(words) as totalWords,avg(words) as agerageWords into wordsCount from blog where author='tom' group by author
-		AggregateIterable<Document> iterable = mc.aggregate(Arrays.asList(match(eq("author", "tom")),
-                group("$author", sum("totalWords", "$words"), avg("averageWords", "$words"), max("maxWords", "$words"), min("minWords", "$words")), out("wordsCount")));
-		printResult("group by author", iterable);
-		
-		//随机取3个文档, 仅返回title和author字段
-		iterable = mc.aggregate(Arrays.asList(sample(3), project(fields(include("title", "author"), excludeId())), out("sample3")));
+
+		AggregateIterable<Document> iterable = mc.aggregate(Arrays.asList(
+				match(in("owner", "tom", "john", "mike")),
+				group("$owner", sum("totalWords", "$words"))));
+		printResult(
+				"like 'select sum(words) from blog where owner in(tom,john,mike) group by owner'",
+				iterable);
+
+		// $match 确定复合条件的文档, 可组合多个条件
+		iterable = mc.aggregate(Arrays.asList(match(and(eq("owner", "tom"),
+				gt("words", 300)))));
+		printResult("$match only", iterable);
+
+		// $sum求和 $avg平均值 $max最大值 $min最小值
+		iterable = mc.aggregate(Arrays.asList(
+				match(in("owner", "tom", "john", "mike")),
+				group("$owner", sum("totalWords", "$words"),
+						avg("averageWords", "$words"),
+						max("maxWords", "$words"), min("minWords", "$words"))));
+		printResult("$sum $avg $max $min", iterable);
+
+		// $out 把聚合结果输出到集合
+		mc.aggregate(Arrays.asList(
+				match(in("owner", "tom", "john", "mike")),
+				group("$owner", sum("totalWords", "$words"),
+						avg("averageWords", "$words"),
+						max("maxWords", "$words"), min("minWords", "$words")),
+				out("wordsCount")));
+		iterable = database.getCollection("wordsCount").aggregate(
+				Arrays.asList(sample(3)));
+		printResult("$out", iterable);
+
+		// 随机取3个文档, 仅返回title和owner字段
+		iterable = mc.aggregate(Arrays.asList(sample(3),
+				project(fields(include("title", "owner"), excludeId()))));
 		printResult("sample(3)", iterable);
-		
-		//从第2个文档开始取2个文档, 仅返回title和author字段
-		iterable = mc.aggregate(Arrays.asList(skip(1), limit(2), project(fields(include("title", "author"), excludeId()))));
+
+		// 从第2个文档开始取2个文档, 仅返回title和owner字段
+		iterable = mc.aggregate(Arrays.asList(skip(1), limit(2),
+				project(fields(include("title", "owner"), excludeId()))));
 		printResult("skip(1), limit(2)", iterable);
-		
+
+		// $lookup 和另一个集合关联
 		database.getCollection("scores").drop();
-		database.getCollection("scores").insertMany(Arrays.asList(new Document("writer", "tom").append("score", 100), new Document("writer", "joe").append("score", 95), new Document("writer", "john").append("score", 80)));
-		
-		iterable = mc.aggregate(Arrays.asList(lookup("scores", "author", "writer", "joinedOutput")));
+		database.getCollection("scores").insertMany(
+				Arrays.asList(
+						new Document("writer", "tom").append("score", 100),
+						new Document("writer", "joe").append("score", 95),
+						new Document("writer", "john").append("score", 80)));
+		iterable = mc.aggregate(Arrays.asList(lookup("scores", "owner",
+				"writer", "joinedOutput")));
 		printResult("lookup", iterable);
-		
-		//拆分comments为单个文档
-		iterable = mc.aggregate(Arrays.asList(match(size("comments", 2)), project(fields(include("comments"), excludeId())), unwind("$comments")));
+
+		// 拆分comments为单个文档
+		iterable = mc.aggregate(Arrays.asList(match(size("comments", 2)),
+				project(fields(include("comments"), excludeId())),
+				unwind("$comments")));
 		printResult("unwind comments", iterable);
-		
-		DistinctIterable<String> it = mc.distinct("author", String.class);
-		it.forEach(new Block<String>() {
-            public void apply(final String str) {
-                System.out.println(str);
-            }
-        });
-		
-		long count = mc.count(Filters.eq("author", "tom"));
+
+		System.out.println("distinct");
+		DistinctIterable<String> di = mc.distinct("owner", String.class);
+		di.forEach(new Block<String>() {
+			public void apply(final String str) {
+				System.out.println(str);
+			}
+		});
+		System.out.println("------------------------------------------------------");
+		System.out.println();
+
+		System.out.println("count");
+		long count = mc.count(Filters.eq("owner", "tom"));
 		System.out.println("count=" + count);
-		
-		String map = "function() { "+   
-	             "var category; " +    
-	             "if ( this.words >= 250 ) "+    
-	             "category = 'Long blogs'; " +  
-	             "else " +  
-	             "category = 'Short blogs'; "+    
-	             "emit(category, {title: this.title});}";  
-	     
-	   String reduce = "function(key, values) { " +  
-	                            "var sum = 0; " +  
-	                            "values.forEach(function(doc) { " +  
-	                            "sum += 1; "+  
-	                            "}); " +  
-	                            "return {blogs: sum};} ";
-	   MapReduceIterable<Document> mi = mc.mapReduce(map, reduce);
-	   mi.forEach(new Block<Document>() {
-           public void apply(final Document str) {
-               System.out.println(str);
-           }
-       });
+		System.out.println("------------------------------------------------------");
+		System.out.println();
+
+		System.out.println("mapreduce");
+		String map = "function() { var category; "
+				+ "if ( this.words >= 280 ) category = 'Long blogs'; "
+				+ "else category = 'Short blogs'; "
+				+ "emit(category, {title: this.title});}";
+
+		String reduce = "function(key, values) { var cnt = 0; "
+				+ "values.forEach(function(doc) { cnt += 1; }); "
+				+ "return {count: cnt};} ";
+		MapReduceIterable<Document> mi = mc.mapReduce(map, reduce);
+		mi.forEach(new Block<Document>() {
+			public void apply(final Document str) {
+				System.out.println(str);
+			}
+		});
+		System.out.println("------------------------------------------------------");
+		System.out.println();
 	}
-	
+
 	public void printResult(String doing, AggregateIterable<Document> iterable) {
 		System.out.println(doing);
-        iterable.forEach(new Block<Document>() {
-            public void apply(final Document document) {
-                System.out.println(document);
-            }
-        });
-        System.out.println("------------------------------------------------------");
-        System.out.println();
+		iterable.forEach(new Block<Document>() {
+			public void apply(final Document document) {
+				System.out.println(document);
+			}
+		});
+		System.out.println("------------------------------------------------------");
+		System.out.println();
 	}
 }
