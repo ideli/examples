cea60e515ae2cddf98d16e14ee6b1c4a27230964
autfish <autfish@163.com>
2016-08-24 14:43:12 +0800

    QuickSort

---
+++ src/main/java/_algorithms/chap01/sort/QuickSort.java
@@ -0,0 +1,12 @@
+package _algorithms.chap01.sort;
+
+/**
+ * 快速排序法
+ * 平均时间复杂度O(NlogN),即O(N*log2N)
+ * (对数的定义: 如果a^x=N(a>0且a≠1),那么数x叫做以a为底N的对数,记作x=logaN,读作以a为底N的对数)
+ * @author autfish
+ *
+ */
+public class QuickSort {
+
+}
