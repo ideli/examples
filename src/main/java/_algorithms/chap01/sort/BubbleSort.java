5f3ad24944cff06a7e386d25799fba3406b1babb
autfish <autfish@163.com>
2016-08-29 21:28:24 +0800

    bubble sort

--- src/main/java/_algorithms/chap01/sort/BubbleSort.java
+++ src/main/java/_algorithms/chap01/sort/BubbleSort.java
@@ -31,7 +31,7 @@ public class BubbleSort {
 		//每一轮后完成一个数字归位, 下一轮要比较的数字个数减1(所以内层循环是j < n - i)
 		int n = numbers.length - 1;
 		int t;
-		for(int i = 0; i < n; i++) {
+		for(int i = 0; i < n - 1; i++) {
 			for(int j = 0; j < n - i; j++) {
 				if(descend) { //从大到小
 					if(numbers[j] < numbers[j + 1]) {
