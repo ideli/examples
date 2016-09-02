5b46ed70485ff272ed45cfedbd17e90f999f6167
autfish <autfish@163.com>
2016-09-02 15:50:53 +0800

    fibonacci

--- src/main/java/_algorithms/chap02/math/Fibonacci.java
+++ src/main/java/_algorithms/chap02/math/Fibonacci.java
@@ -16,7 +16,7 @@ public class Fibonacci extends RecursiveTask<Long> {
         long begin = System.nanoTime();
         long f = Fibonacci.fib(n);
         long end = System.nanoTime();
-        System.out.println("第" + n + "个斐波那契数是" + Fibonacci.fib(n) + ", 耗时" + TimeUnit.NANOSECONDS.toMillis(end - begin) + "毫秒");
+        System.out.println("第" + n + "个斐波那契数是" + f + ", 耗时" + TimeUnit.NANOSECONDS.toMillis(end - begin) + "毫秒");
         */
         long n = 50;
         long begin = System.nanoTime();
@@ -24,7 +24,12 @@ public class Fibonacci extends RecursiveTask<Long> {
         ForkJoinPool pool = new ForkJoinPool();
         long f = pool.invoke(fibonacci);
         long end = System.nanoTime();
-        System.out.println("第" + n + "个斐波那契数是" + Fibonacci.fib(n) + ", 耗时" + TimeUnit.NANOSECONDS.toMillis(end - begin) + "毫秒");
+        System.out.println("第" + n + "个斐波那契数是" + f + ", 耗时" + TimeUnit.NANOSECONDS.toMillis(end - begin) + "毫秒");
+
+        begin = System.nanoTime();
+        f = Fibonacci.calcWithoutRecursion(n);
+        end = System.nanoTime();
+        System.out.println("第" + n + "个斐波那契数是" + f + ", 耗时" + TimeUnit.NANOSECONDS.toMillis(end - begin) + "毫秒");
     }
 
     long n;
@@ -34,7 +39,7 @@ public class Fibonacci extends RecursiveTask<Long> {
 
     public Long compute() {
         if(n <= 10) {  //小于10不再分解
-            return Fibonacci.fib(n);
+            return Fibonacci.calc(n);
         }
         Fibonacci f1 = new Fibonacci(n - 1);  //分解出计算n-1斐波那契数的子任务
         f1.fork();  //由ForkJoinPool分配线程执行子任务
@@ -42,11 +47,30 @@ public class Fibonacci extends RecursiveTask<Long> {
         return f2.compute() + f1.join();
     }
 
-    public static long fib(long n) {
+    public static long calc(long n) {
+        if(n < 0)
+            return 0;
         if(n == 0 || n == 1) {
             return n;
         } else {
-            return fib(n - 1) + fib(n - 2);
+            return calc(n - 1) + calc(n - 2);
         }
+    }
+
+    public static long calcWithoutRecursion(long n) {
+        if(n < 0)
+            return 0;
+        if(n == 0 || n == 1) {
+            return n;
+        }
+        long fib = 0;
+        long fibOne = 1;
+        long fibTwo = 1;
+        for(long i = 2; i < n; i++) {
+            fib = fibOne + fibTwo;
+            fibTwo = fibOne;
+            fibOne = fib;
+        }
+        return fib;
     }
 }
