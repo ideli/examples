177802045458bfa9a69f73a45c536f8baa9ef02a
autfish <autfish@163.com>
2016-09-20 17:03:48 +0800

    shell sort

--- src/main/java/_algorithms/chap01/sort/ShellSort.java
+++ src/main/java/_algorithms/chap01/sort/ShellSort.java
@@ -11,6 +11,10 @@ public class ShellSort {
         this.numbers = numbers;
     }
 
+    /**
+     * 对数组分组并对每个组做直接插入排序, 完成后缩小组的数量, 重复插入排序, 直到缩小到一个组
+     * 第一次分组数: section = n/2 == 0 ? n/2 : n/2+1, 分组规则: 每隔n/2挑一个数, 即[x]和[x+n/2]为一组
+     */
     public void sort() {
         int section = this.numbers.length / 2;
         int j;
@@ -34,7 +38,7 @@ public class ShellSort {
     }
 
     public static void main(String[] args) {
-        int[] numbers = new int[] { 4, 3, 6, 2, 7, 1, 5 };
+        int[] numbers = new int[] { 4, 3, 6, 2, 7, 1, 5, 8 };
         System.out.print("排序前: ");
         for(int x = 0; x < numbers.length; x++) {
             System.out.print(numbers[x] + "  ");
